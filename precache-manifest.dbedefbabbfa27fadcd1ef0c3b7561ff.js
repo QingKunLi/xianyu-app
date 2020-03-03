9058d218822c9259426f087057b6a66ec0318ca4
QingKunLi <lqk1997@mail.ustc.edu.cn>
2020-03-03 09:42:02 +0800

    deploy

---
+++ precache-manifest.dbedefbabbfa27fadcd1ef0c3b7561ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e0c90f28bf92552fe93",
+    "url": "/xianyu-app/css/app.266538f5.css"
+  },
+  {
+    "revision": "9c00557e0644e9f67720c8fbddae44eb",
+    "url": "/xianyu-app/img/logo.9c00557e.png"
+  },
+  {
+    "revision": "6d848c5ece0a165fedb5e1eb71cf8af8",
+    "url": "/xianyu-app/index.html"
+  },
+  {
+    "revision": "4e0c90f28bf92552fe93",
+    "url": "/xianyu-app/js/app.0fa0342f.js"
+  },
+  {
+    "revision": "d233ef3c9b3c2c8263f4",
+    "url": "/xianyu-app/js/chunk-vendors.ffb21e19.js"
+  },
+  {
+    "revision": "87e36ed761a5ce601b87d050620f259b",
+    "url": "/xianyu-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/xianyu-app/robots.txt"
+  }
+]);
